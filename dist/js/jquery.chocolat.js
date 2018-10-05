f90b82a2024046806c8f221189d9ee14ac06723d
gilbertorey <gilberto@gilbertorey.com>
2018-10-05 02:47:52 -0500

    Update jquery.chocolat.js

--- dist/js/jquery.chocolat.js
+++ dist/js/jquery.chocolat.js
@@ -717,7 +717,7 @@
         timerDebounce     : false,
         images            : [],
         enableZoom        : true,
-        imageSource       : style,
+        imageSource       : "background-image",
         afterInitialize   : function () {},
         afterMarkup       : function () {},
         afterImageLoad    : function () {},
